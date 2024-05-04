d1e72172b409fe0d51537494a09d516fc2489eef
jlsalvat <salvat@unice.fr>
2024-05-04 13:04:50 +0200

    Update main.c

--- Starfield/main.c
+++ Starfield/main.c
@@ -25,16 +25,18 @@ int main(){
 //deuxième programme qui affiche une fenêtre avec un rond rouge et qui gère les évènements liés à la souris et au clavier (flèches)
 //permet de d'afficher un rond rouge et de le déplacer
 #include <SFML/Graphics.h>
+
+
 int main()
 {
 	sfRenderWindow* window;
 	sfEvent event;
-	sfCircleShape* rond;
+	sfRectangleShape* rect;
 	sfVector2f position = { 0,0 };
 	int dy = 10,dx=10;
-	rond= sfCircleShape_create();
-	sfCircleShape_setFillColor(rond, sfRed);
-	sfCircleShape_setRadius(rond, 30);
+	rect= sfRectangleShape_create();
+	sfRectangleShape_setFillColor(rect, sfRed);
+	sfRectangleShape_setSize(rect, (sfVector2f) { 50, 50 });
 	window = sfRenderWindow_create((sfVideoMode) { 280, 280, 32 }, "Hello!",  sfClose, NULL);
 	while (sfRenderWindow_isOpen(window)) {
 		// Process events 
@@ -57,21 +59,21 @@ int main()
 					position.x += dx;
 			}
 		}
-		sfCircleShape_setPosition(rond, position);
+		sfRectangleShape_setPosition(rect, position);
 		sfRenderWindow_clear(window, sfWhite);
-		sfRenderWindow_drawCircleShape(window, rond, NULL);
+		sfRenderWindow_drawRectangleShape(window, rect, NULL);
 		// Update the window 
 		sfRenderWindow_display(window);
 		// temps de rafraichissement pour pas trop charger le GPU
 		sfSleep((sfTime) { 20000 });
 	}
 	// Cleanup resources 
-	sfCircleShape_destroy(rond);
+	sfRectangleShape_destroy(rect);
 	sfRenderWindow_destroy(window);
 	return 0;
 }
-*/
-/*
+
+
 //troisième programme qui permet d'afficher du texte (click ou A) ou un rond vert en fonction de la variable toggle.
 
 #define _CRT_SECURE_NO_WARNINGS
@@ -135,4 +137,120 @@ int main()
 	sfRenderWindow_destroy(window);
 	return 0;
 }
+
+//quatrième programme qui permet d'afficher du texte (click ou A) , une image et émettre un son
+
+#include <SFML/Audio.h>
+#include <SFML/Graphics.h>
+#include <stdlib.h>
+#include <stdio.h>
+//Pour pouvoir lancer ce jeu vous devez installer https://www.openal.org/downloads/ sur votre PC
+
+#define HAUTEUR 600
+#define LARGEUR 800
+int main()
+{
+	sfVideoMode mode = { LARGEUR, HAUTEUR, 32 };
+	sfRenderWindow* window;
+	sfTexture* texture;
+	sfSprite* sprite;
+	sfFont* font;
+	sfText* text;
+	sfMusic* music;
+	sfEvent event;
+	sfClock* clk;
+
+	//mes variables
+	char tabText[100] = "C'est parti : un clic pour voir";
+	int cpt = 0;
+	sfVector2f pos = { 100,100 }, posImage = { LARGEUR / 2,HAUTEUR / 2 };
+
+	//Create the main window 
+	window = sfRenderWindow_create(mode, "SFML window", sfResize | sfClose, NULL);
+	// open FONT 
+	font = sfFont_createFromFile("ressources/arial.ttf");
+	// Load a sprite to display 
+	texture = sfTexture_createFromFile("ressources/sfmljl_icon.png", NULL);
+	sprite = sfSprite_create();
+	sfSprite_setTexture(sprite, texture, sfTrue);
+	sfSprite_setPosition(sprite, posImage);
+	// Create a graphical text to display 
+	text = sfText_create();
+	sfText_setFont(text, font);
+	sfText_setCharacterSize(text, 20);
+	pos.x = pos.x;
+	pos.y = pos.y + 100;
+	sfText_setPosition(text, pos);
+	sfText_setString(text, tabText);
+
+	// Load a music to play in loop
+	music = sfMusic_createFromFile("ressources/sound.wav");
+	if (!music) {
+		printf("ERREUR : ouverture du fichier \'ressources/sound.wav\'");
+		return EXIT_FAILURE;
+	}
+	sfTime dur = sfMusic_getDuration(music);
+	sfTimeSpan timespan = { 0, dur };
+	sfMusic_setLoopPoints(music, timespan);
+
+	//lance timer pour le son
+	clk = sfClock_create();
+	// Start the game loop 
+	while (sfRenderWindow_isOpen(window))
+	{
+		// Process events
+		while (sfRenderWindow_pollEvent(window, &event))
+		{
+			// Close window : exit or Escape
+			if (event.type == sfEvtClosed || event.key.code == sfKeyEscape)
+				sfRenderWindow_close(window);
+			//click on mouse 
+			if (event.type == sfEvtMouseButtonPressed) {
+				pos.x = event.mouseButton.x;
+				pos.y = event.mouseButton.y;
+				sprintf(tabText, "CLICK %d", cpt++);
+				//on lance la musique
+				sfMusic_play(music);
+				sfClock_restart(clk);
+			}
+			//use of arrow for move the image
+			if (event.key.code == sfKeyLeft)
+				posImage.x -= 10;
+			if (event.key.code == sfKeyRight)
+				posImage.x += 10;
+			if (event.key.code == sfKeyUp)
+				posImage.y -= 10;
+			if (event.key.code == sfKeyDown)
+				posImage.y += 10;
+		}
+		sfSprite_setPosition(sprite, posImage);
+		sfText_setString(text, tabText);
+		sfText_setPosition(text, pos);
+		//arrêt de la musique au bout de 2s
+		sfTime temps = sfClock_getElapsedTime(clk);
+		if (temps.microseconds > 1000000) {
+			sfMusic_stop(music);
+		}
+		//Clear the screen 
+		sfRenderWindow_clear(window, sfBlack);
+		// Draw the sprite 
+		sfRenderWindow_drawSprite(window, sprite, NULL);
+		// Draw the texts 
+		sfRenderWindow_drawText(window, text, NULL);
+		// Update the window 
+		sfRenderWindow_display(window);
+		// temps de rafraichissement pour pas trop charger le GPU
+		sfTime time_wait = { 20000 };// 20 ms
+		sfSleep(time_wait);
+	}
+	// Cleanup resources 
+	sfMusic_destroy(music);
+	sfText_destroy(text);
+	sfFont_destroy(font);
+	sfSprite_destroy(sprite);
+	sfTexture_destroy(texture);
+	sfRenderWindow_destroy(window);
+	return EXIT_SUCCESS;
+}
 */
+
